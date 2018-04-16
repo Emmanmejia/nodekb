6540173dd67c28fdba7d13d4e19c55cc17567def
Czar Emmanuel Mejia <czar.mejia@obf.ateneo.edu>
2018-04-16 14:15:41 +0800

    updated

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
+/*
 module.exports = {
   database:'mongodb://localhost:27017/nodekb',
   secret: 'yoursecret'
-}
+} */
 
-/*
+
 module.exports = {
   database:'mongodb://admin:password@ds239309.mlab.com:39309/nodekb',
   secret: 'yoursecret'
 }
-*/
+
 
 
